aa2e06e64ef3f15fbf42f45c616700181783aad5
AAlexandrovic <92386024+AAlexandrovic@users.noreply.github.com>
2023-03-10 11:11:32 +0300

    Create getTest.php

---
+++ getTest.php
@@ -0,0 +1,11 @@
+php
+header("Access-Control-Allow-Origin: *");
+include 'auth.php';
+
+$array = ($_GET);
+$test = explode('/', $array['width']);
+$name = explode('.', $test[2]);
+$name = $name[0];
+$lead = $test[5];
+
+(new ApiService())->getContacts($name,$lead);
